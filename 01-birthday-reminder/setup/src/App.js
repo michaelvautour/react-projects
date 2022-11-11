4f6cb48bdfe3bcb8f1a2d725f107fc270f02fe1a
Michael Vautour <michael.e.vautour@gmail.com>
2022-11-10 19:20:51 -0700

    list works without typescript!, need to fix, also need to add clear function on list

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import { data } from "./data";
+import List from "./List";
+import { Person } from "./types";
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+
+  console.log(people);
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthday's today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear List</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
---
+++ 01-birthday-reminder/setup/src/types.tsx
@@ -0,0 +1,8 @@
+export type Person = {
+  people: {
+    id: number;
+    name: string;
+    age: string;
+    image: string;
+  };
+};
