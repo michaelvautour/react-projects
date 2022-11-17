e35e2f004be756c4505e6eff8c0b923b4ad87e8c
Michael Vautour <michael.e.vautour@gmail.com>
2022-11-16 19:29:47 -0700

    useRef, useState, & getBoundingClientRect functionality

--- 10-grocery-bud/setup/src/Alert.tsx
+++ 10-grocery-bud/setup/src/Alert.tsx
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
-const Alert = ({ type, msg, removeAlert, list }) => {
+const Alert = ({ type, msg, removeAlert, list }: any) => {
   useEffect(() => {
     const timeout = setTimeout(() => {
       removeAlert();
     }, 3000);
     return () => clearTimeout(timeout);
   }, [list]);
+  // above [list] allows it so the timer resets to 3 seconds if multiple actions happen at once
   return <p className={`alert alert-${type}`}>{msg}</p>;
 };
 
---
+++ 11-navbar/setup/src/Navbar.tsx
@@ -0,0 +1,69 @@
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
+import Sidebar from "./Sidebar";
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const linksContainerRef = useRef<HTMLInputElement>(null);
+  const linksRef = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current!.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current!.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current!.style.height = "0px";
+    }
+    console.log(linksHeight);
+  }, [showLinks]);
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" />
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
+            <FaBars />
+          </button>
+        </div>
+        {/*
+         below using terinary operator to show/hide example:
+        <div
+          className={`${
+            showLinks ? "show-container links-container" : "links-container"
+          }`}
+        > */}
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
